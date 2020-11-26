86cf8f1e3af6da7efabe6e8a64649117b4028718
Juniza Permata Nugraheni <72010002+junizapermatanugraheni@users.noreply.github.com>
2020-11-26 08:14:03 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
 //koneksi ke database
 try{
-	$myPDO = new PDO("pgsql:host=localhost,dbname=juniza","postgres","1234");
+	$myPDO = new PDO("pgsql:host=localhost;dbname=juniza","postgres","1234");
 	echo "Connected to PostgreSQL with PDO";
 }catch(PDOException $e){
 	echo "Koneksi database gagal: " . $e->getMessage();
